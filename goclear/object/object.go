e618b7ffada50e80930a75e8745f1f466b3820e9
Adam Troup <ajtroup1@crimson.ua.edu>
2025-01-15 23:37:16 -0600

    index expression evaluated @pg.179

--- goclear/object/object.go
+++ goclear/object/object.go
@@ -17,6 +17,7 @@ const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
 	STRING_OBJ  = "STRING"
+	ARRAY_OBJ   = "ARRAY"
 
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 
@@ -102,3 +103,23 @@ type Builtin struct {
 
 func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
 func (b *Builtin) Inspect() string  { return "builtin function" }
+
+type Array struct {
+	Elements []Object
+}
+
+func (ao *Array) Type() ObjectType { return ARRAY_OBJ }
+func (ao *Array) Inspect() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, el := range ao.Elements {
+		elements = append(elements, el.Inspect())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}

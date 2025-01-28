f6ff0445d33de21582912b5c841fc93454e780d6
Adam Troup <ajtroup1@crimson.ua.edu>
2025-01-27 22:19:29 -0600

    actually evaluating conditionals

--- compiler/src/vm/vm.go
+++ compiler/src/vm/vm.go
@@ -88,6 +88,18 @@ func (vm *VM) Run() error {
 				return err
 			}
 
+		case code.OpJump:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip = pos - 1
+
+		case code.OpJumpNotTruthy:
+			pos := int(code.ReadUint16(vm.instructions[ip+1:]))
+			ip += 2
+			condition := vm.pop()
+			if !isTruthy(condition) {
+				ip = pos - 1
+			}
+
 		// Pop the top of the stack
 		// Used after every expression statement
 		case code.OpPop:
@@ -96,6 +108,15 @@ func (vm *VM) Run() error {
 	}
 
 	return nil
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj := obj.(type) {
+	case *object.Boolean:
+		return obj.Value
+	default:
+		return true
+	}
 }
 
 // Found an infix expression, execute it according to the operator and data types
